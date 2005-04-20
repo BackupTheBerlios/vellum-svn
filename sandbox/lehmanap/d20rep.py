b3d45684f424f34994ce46c56fc87c640f851088
lehmanap <lehmanap@dac6abc3-03f5-0310-8df6-960e0b1e0653>
2005-04-20 04:46:18 +0000

    Cleaner code, conditionals handled nicer.

--- sandbox/lehmanap/d20rep.py
+++ sandbox/lehmanap/d20rep.py
@@ -1,77 +1,77 @@
 from rep import *
 
 class D20AbilityScore( Stat ):
-    def __init__( self, name, number, conditions ):
-        import math
-        super( D20AbilityScore, self ).__init__( name, number, conditions )
-        self.mod = lambda: int( math.floor( ( int( self ) - 10 ) / 2 ) )
+	def __init__( self, name, number ):
+		import math
+		super( D20AbilityScore, self ).__init__( name, number )
+		self.mod = lambda: int( math.floor( ( int( self ) - 10 ) / 2 ) )
 
 class D20Character( Character ):
+	def __init__( self, name, attrs=None ):
+		super( D20Character, self ).__init__( name, attrs )
+		self.FEATS = { }
 
-    def beginCombat( self ):
-        self.DEX.mod.addTarget( self.AC, Conditions( ( ), "and" ) )
+	def beginCombat( self ):
+		self.DEX.mod.addTarget( self.AC, Conditions( ( ), "and" ) )
 
-    def endCombat( self ):
-        self.DEX.mod.removeTarget( self.AC )
+	def endCombat( self ):
+		self.DEX.mod.removeTarget( self.AC )
 
-    def readyWeapon( self, weapon ):
-        #This will need fixing.  It's just for testing purposes.  FIXME
-        self.ATT.situation = weapon
+	def attack( self, foe, weapon ):
+		#This will need fixing.  It's just for testing purposes.  FIXME
+		self.ATT.situation = Attack( self, foe, weapon )
 
-    def unreadyWeapon( self ):
-        #This will need fixing.  It's just for testing purposes.  FIXME
-        self.ATT.situation = None
+	def unreadyWeapon( self ):
+		#This will need fixing.  It's just for testing purposes.  FIXME
+		self.ATT.situation = None
 
-    #Properties
-    def setAbilityScore( self, tag, name, number, modsprovided=None ):
-        try:
-            self.attrs[ tag ].number = number
-        except:
-            self.attrs[ tag ] = D20AbilityScore( name, number, self.conditions )
+	#Properties
+	def setAbilityScore( self, tag, name, number, modsprovided=None ):
+		try:
+			self.attrs[ tag ].number = number
+		except:
+			self.attrs[ tag ] = D20AbilityScore( name, number )
 
-    attTypes = { 
-                #Basic Attributes
-                'STR' : ( setAbilityScore, 'Strength', { } ),
-                'DEX' : ( setAbilityScore, 'Dexterity', { } ),
-                'CON' : ( setAbilityScore, 'Constitution', { } ),
-                'INT' : ( setAbilityScore, 'Intelligence', { } ),
-                'WIS' : ( setAbilityScore, 'Wisdom', { } ),
-                'CHA' : ( setAbilityScore, 'Charisma', { } ),
-                #END Basic Attributes
+	attTypes = { 
+				#Basic Attributes
+				'STR' : ( setAbilityScore, 'Strength', { }, 0 ),
+				'DEX' : ( setAbilityScore, 'Dexterity', { }, 0 ),
+				'CON' : ( setAbilityScore, 'Constitution', { }, 0 ),
+				'INT' : ( setAbilityScore, 'Intelligence', { }, 0 ),
+				'WIS' : ( setAbilityScore, 'Wisdom', { }, 0 ),
+				'CHA' : ( setAbilityScore, 'Charisma', { }, 0 ),
+				#END Basic Attributes
 
-                #Calculated Attributes
-                'AC' : ( Character.setAttribute, 'Armor Class', { } ),
-                'ATT' : ( Character.setAttribute, 'Attack Bonus', { 'STR': ( "MELEE IN SOURCE TYPES", "and" ), 'DEX': ( "RANGE IN SOURCE TYPES", "and" )  } ),
-                'INIT' : ( Character.setAttribute, 'Initiative', { } ),
-                'SPEED' : ( Character.setAttribute, 'Movement Speed', { } ),
-                }
+				#Calculated Attributes
+				'AC' : ( Character.setAttribute, 'Armor Class', { }, 10 ),
+				'ATT' : ( Character.setAttribute, 'Attack Bonus', 
+					{ 
+						'STR': ( "MELEE IN TOOL TYPES", "WPN_FINESSE NOT IN SUBJECT FEATS", "and" ),
+						'DEX': ( "RANGE IN TOOL TYPES", "WPN_FINESSE NOT IN SUBJECT FEATS", "and" )
+					}, -4 ),
+				'INIT' : ( Character.setAttribute, 'Initiative', { }, 0 ),
+				'SPEED' : ( Character.setAttribute, 'Movement Speed', { }, 30 ),
+				}
 
 if __name__ == "__main__":
-    class Source:
-        pass
-    class Weapon:
-        def __init__( self ):
-            self.SOURCE = Source( )
-    sword = Weapon( )
-    sword.SOURCE.TYPES = [ 'MELEE', 'SLASHING', 'PIERCING', 'CORPORIAL' ]
-    bow = Weapon( )
-    bow.SOURCE.TYPES = [ 'RANGE', 'PIERCING', 'CORPORIAL' ]
-    bob = D20Character( "Bob", conditions )
-    dexmod = ModifierFactory( "Cause", 2 )
-    print "AC before Dex mod"
-    print bob.AC
-    dexmod.addTarget( bob.DEX, Conditions( ( ), "and" ) )
-    bob.beginCombat( )
-    print "After"
-    print bob.AC
-    print "ATT before weapon"
-    print bob.ATT
-    bob.readyWeapon( sword )
-    print "After Sword"
-    print bob.ATT
-    bob.unreadyWeapon( )
-    print "Sword Removed"
-    print bob.ATT
-    bob.readyWeapon( bow )
-    print "With Bow"
-    print bob.ATT
+	sword = Weapon( 'Sword', [ 'MELEE', 'SLASHING', 'PIERCING', 'CORPORIAL' ] )
+	bow = Weapon( 'Bow', [ 'RANGE', 'PIERCING', 'CORPORIAL' ] )
+	bob = D20Character( "Bob", { 'STR': 13, 'DEX':15, 'CON':12, 'INT':10, 'WIS':8, 'CHA':18 } )
+	dexmod = ModifierFactory( "Cause", 2 )
+	print "AC before Dex mod"
+	print bob.AC
+	dexmod.addTarget( bob.DEX, Conditions( ( ), "and" ) )
+	bob.beginCombat( )
+	print "After"
+	print bob.AC
+	print "ATT before weapon"
+	print bob.ATT
+	bob.attack( bob, sword )
+	print "After Sword"
+	print bob.ATT
+	bob.unreadyWeapon( )
+	print "Sword Removed"
+	print bob.ATT
+	bob.attack( bob, bow )
+	print "With Bow"
+	print bob.ATT
--- sandbox/lehmanap/rep.py
+++ sandbox/lehmanap/rep.py
@@ -1,261 +1,280 @@
 conditions = { True: lambda a: True }
 class MyObject( object ):
-    def __repr__( self ):
-        return "%s %s" % ( str( self ), super( MyObject, self ).__repr__( ) )
+	def __repr__( self ):
+		return "%s %s" % ( str( self ), super( MyObject, self ).__repr__( ) )
+
+class Weapon( MyObject ):
+	def __init__( self, name, types ):
+		self.name = name
+		self.TYPES = types
+
+	def __str__( self ):
+		return "%s %s" % ( self.name, self.TYPES )
+
+class Situation( MyObject ):
+	def __init__( self, subject, object, tool ):
+		self.subject = subject
+		self.object = object
+		self.tool = tool
+
+	def __str__( self ):
+		"%s is doing something with %s using a(n) %s" % ( self, subject, self.object, self.tool )
+
+class Attack( Situation ):
+	def __str__( self ):
+		"%s attacks %s using a(n) %s" % ( self, subject, self.object, self.tool )
 
 class Condition( MyObject ):
-    def __init__( self, attToCheck, valToCheck, partToCheck, op ):
-        self.attToCheck = attToCheck.upper( )
-        self.partToCheck = partToCheck.upper( )
-        self.valToCheck = valToCheck.upper( )
-        self.op = op.lower( )
+	def __init__( self, attToCheck, valToCheck, partToCheck, op ):
+		self.attToCheck = attToCheck.upper( )
+		self.partToCheck = partToCheck.lower( )
+		self.valToCheck = valToCheck.upper( )
+		self.op = op.lower( )
 
-    def check( self, circumstance ):
-        vals = getattr( getattr( circumstance, self.partToCheck ), self.attToCheck, [ self.valToCheck ] )
-        if self.op == "in":
-            return self.valToCheck in vals
-        elif self.op == "not in":
-            return self.valToCheck not in vals
-        
-    def __str__( self ):
-        return "( %s %s %s's %s )" % ( self.valToCheck, self.op, self.partToCheck, self.attToCheck )
-        
+	def check( self, circumstance ):
+		vals = getattr( getattr( circumstance, self.partToCheck ), self.attToCheck, [ self.valToCheck ] )
+		if self.op == "in":
+			return self.valToCheck in vals
+		elif self.op == "not in":
+			return self.valToCheck not in vals
+		
+	def __str__( self ):
+		return "( %s %s %s's %s )" % ( self.valToCheck, self.op, self.partToCheck, self.attToCheck )
+		
 class Conditions( MyObject ):
-    def __init__( self, conditions, op ):
-        self.conditions = [ ]
-        for c in conditions:
-            if isinstance( c, str ):
-                c = c.split( )
-                self.conditions.append( Condition( c[ -1 ], c[ 0 ], c[ -2 ], " ".join( c[ 1:-2 ] ) ) )
-            elif isinstance( c, tuple ):
-                self.conditions.append( Conditions( c[ :-1 ], c[ -1 ] ) )
-        self.op = op.lower( )
+	def __init__( self, conditions, op ):
+		self.conditions = [ ]
+		for c in conditions:
+			if isinstance( c, str ):
+				c = c.split( )
+				self.conditions.append( Condition( c[ -1 ], c[ 0 ], c[ -2 ], " ".join( c[ 1:-2 ] ) ) )
+			elif isinstance( c, tuple ):
+				self.conditions.append( Conditions( c[ :-1 ], c[ -1 ] ) )
+		self.op = op.lower( )
 
-    def __str__( self ):
-        return ( " %s " % self.op ).join( [ str( c ) for c in self.conditions ] )
-        
-    def check( self, circumstance ):
-        for c in self.conditions:
-            truth = c.check( circumstance )
-            if self.op == "and" and truth == False:
-                return False
-            elif self.op == "or" and truth == True:
-                return True
-        if self.op == "and":
-            return True
-        elif self.op == "or":
-            return False
+	def __str__( self ):
+		return ( " %s " % self.op ).join( [ str( c ) for c in self.conditions ] )
+		
+	def check( self, circumstance ):
+		for c in self.conditions:
+			truth = c.check( circumstance )
+			if self.op == "and" and truth == False:
+				return False
+			elif self.op == "or" and truth == True:
+				return True
+		if self.op == "and":
+			return True
+		elif self.op == "or":
+			return False
 
 
 class Modifier( MyObject ):
-    def __init__( self, parent, target, conditions ):
-        self.parent = parent
-        self.target = target
-        self.conditions = conditions
+	def __init__( self, parent, target, conditions ):
+		self.parent = parent
+		self.target = target
+		self.conditions = conditions
 
-    def __add__( self, other ):
-        try:
-            return int( self ) + other
-        except:
-            return other
-    __radd__ = __add__
+	def __add__( self, other ):
+		try:
+			return int( self ) + other
+		except:
+			return other
+	__radd__ = __add__
 
-    def __int__( self ):
-        if self.conditions.check( self.target.situation ):
-        #If the circumstances are right, add me to the value
-            return int( self.parent )
-        else:
-            raise "I don't modify %s under these circumstances" % self.target.name
+	def __int__( self ):
+		if self.conditions.check( self.target.situation ):
+		#If the circumstances are right, add me to the value
+			return int( self.parent )
+		else:
+			raise "I don't modify %s under these circumstances" % self.target.name
 
-    def __str__( self ):
-        try:
-            num = int( self )
-            return str( self.parent )
-        except:
-            return "\b"
+	def __str__( self ):
+		try:
+			num = int( self )
+			return str( self.parent )
+		except:
+			return "\b"
 
-    #Properties
-    def getKey( self ):
-        return self.parent.key
-    key = property( fget=getKey, doc="The unique key for this Modifier's ModifierFactory" )
-    
-    def getNumber( self ):
-        return self.parent.number
-    number = property( fget=getNumber, doc="This Attributes base value" )
+	#Properties
+	def getKey( self ):
+		return self.parent.key
+	key = property( fget=getKey, doc="The unique key for this Modifier's ModifierFactory" )
+	
+	def getNumber( self ):
+		return self.parent.number
+	number = property( fget=getNumber, doc="This Attributes base value" )
 
 class ModifierFactory( MyObject ):
-    def __init__( self, cause, number ):
-        self.cause = cause
-        self.targets = { }
-        self.number = number
-        self.targetId = None
+	def __init__( self, cause, number ):
+		self.cause = cause
+		self.targets = { }
+		self.number = number
+		self.targetId = None
 
-    def __int__( self ):
-        return self.number
+	def __int__( self ):
+		return self.number
 
-    def __str__( self ):
-        number = self.number
-        return "%s%s ( from %s )" % ( number > -1 and "+" or "", number, str( self.cause ) )
+	def __str__( self ):
+		number = self.number
+		return "%s%s ( from %s )" % ( number > -1 and "+" or "", number, str( self.cause ) )
 
-    def addTarget( self, target, conditions ):
-        mod = Modifier( self, target, conditions )
-        self.targets[ target.key ] = mod
-        target.modifiers[ self.key ] = mod
+	def addTarget( self, target, conditions ):
+		mod = Modifier( self, target, conditions )
+		self.targets[ target.key ] = mod
+		target.modifiers[ self.key ] = mod
 
-    def removeTarget( self, target ):
-        del self.targets[ target.key ]
-        del target.modifiers[ self.key ]
+	def removeTarget( self, target ):
+		del self.targets[ target.key ]
+		del target.modifiers[ self.key ]
 
-    #Properties
-    def getKey( self ):
-        return "%s: %s %s" % ( self.__class__.__name__, getattr( self.cause, 'key', self.cause ), " ".join( self.targets.keys( ) ) )
-    key = property( fget=getKey, doc="The unique key for this ModifierFactory" )
-    
-    def getNumber( self ):
-        return self._number( )
+	#Properties
+	def getKey( self ):
+		return "%s: %s %s" % ( self.__class__.__name__, getattr( self.cause, 'key', self.cause ), " ".join( self.targets.keys( ) ) )
+	key = property( fget=getKey, doc="The unique key for this ModifierFactory" )
+	
+	def getNumber( self ):
+		return self._number( )
 
-    def setNumber( self, number ):
-        self._number = callable( number ) and number or ( lambda: number )
+	def setNumber( self, number ):
+		self._number = callable( number ) and number or ( lambda: number )
 
-    number = property( fget=getNumber, fset=setNumber, doc="This Attributes base value" )
+	number = property( fget=getNumber, fset=setNumber, doc="This Attributes base value" )
 
 class Attribute( MyObject ):
-    lastkey = 0
-    
-    def __init__( self, name, number, conditions ):
-        self.name = name
-        self.number = number
-        self.modifiers = { }
-        self.key = "%s %s" % ( self.__class__.__name__, self.lastkey )
-        Attribute.lastkey += 1
-        self.situation = True
-        self.conditions = conditions
+	lastkey = 0
+	
+	def __init__( self, name, number ):
+		self.name = name
+		self.number = number
+		self.modifiers = { }
+		self.key = "%s %s" % ( self.__class__.__name__, self.lastkey )
+		Attribute.lastkey += 1
+		self.situation = True
 
-    def __add__( self, other ):
-        return int( self ) + other
-    __radd__ = __add__
+	def __add__( self, other ):
+		return int( self ) + other
+	__radd__ = __add__
 
-    def __cmp__( self, other ):
-        import math
-        diff = self - other
-        return diff and diff / math.fabs( diff ) or diff
+	def __cmp__( self, other ):
+		import math
+		diff = self - other
+		return diff and diff / math.fabs( diff ) or diff
 
-    def __int__( self ):
-        return self.number + sum( self.modifiers.values( ) )
+	def __int__( self ):
+		return self.number + sum( self.modifiers.values( ) )
 
-    def __str__( self ):
-        return "%s: " % self.name + " ".join( [ "%s" % self.number ] + [ str( mod ) for mod in self.modifiers.values( ) ] ) + " = %s" % int( self )
+	def __str__( self ):
+		return "%s: " % self.name + " ".join( [ "%s" % self.number ] + [ str( mod ) for mod in self.modifiers.values( ) ] ) + " = %s" % int( self )
 
-    def __sub__( self, other ):
-        return int( self ) - other
-    __rsub__ = __sub__
+	def __sub__( self, other ):
+		return int( self ) - other
+	__rsub__ = __sub__
 
-    def resolveForSituation( self, situation ):
-        sit = self.situation
-        self.situation = situation
-        ammount = int( self )
-        self.situation = sit
-        return ammount
+	def resolveForSituation( self, situation ):
+		sit = self.situation
+		self.situation = situation
+		ammount = int( self )
+		self.situation = sit
+		return ammount
 
-    #Properties
-    def getNumber( self ):
-        return self._number( )
+	#Properties
+	def getNumber( self ):
+		return self._number( )
 
-    def setNumber( self, number ):
-        self._number = callable( number ) and number or ( lambda: number )
+	def setNumber( self, number ):
+		self._number = callable( number ) and number or ( lambda: number )
 
-    number = property( fget=getNumber, fset=setNumber, doc="This Attributes base value" )
+	number = property( fget=getNumber, fset=setNumber, doc="This Attributes base value" )
 
 class Stat( Attribute ):
-    def __init__( self, name, number, conditions, modprovided=None ):
-        super( Stat, self ).__init__( name, number, conditions )
-        self.mod = modprovided or self.number
+	def __init__( self, name, number, modprovided=None ):
+		super( Stat, self ).__init__( name, number )
+		self.mod = modprovided or self.number
 
-    def applyMods( self ):
-        for mod in self.modprovided:
-            mod.addToTargets( )
+	def applyMods( self ):
+		for mod in self.modprovided:
+			mod.addToTargets( )
 
-    def revokeMods( self ):
-        for mod in self.modprovided:
-            mod.removeFromTargets( )
+	def revokeMods( self ):
+		for mod in self.modprovided:
+			mod.removeFromTargets( )
 
-    #Properties
-    def getMod( self ):
-        return self.modprovided
+	#Properties
+	def getMod( self ):
+		return self.modprovided
 
-    def setMod( self, value ):
-        try:
-            self.modprovided.number = value
-        except:
-            self.modprovided = ModifierFactory( self, value )
-    mod = property( fget=getMod, fset=setMod, doc="The modifier this Stat provides" )
+	def setMod( self, value ):
+		try:
+			self.modprovided.number = value
+		except:
+			self.modprovided = ModifierFactory( self, value )
+	mod = property( fget=getMod, fset=setMod, doc="The modifier this Stat provides" )
 
 class CharacterMetaclass( type ):
-    def __init__( clss, name, bases, dictionary ):
-        def generateProperty( attrType, attrTag, attrName ):
-            """
-            Because lambdas do not rememebr their scope, if the stack is overwritten.
-            """
-            return ( lambda self: self.attrs[ attrTag ] ), ( lambda self, value: attrType( self, attrTag, attrName, value ) )
+	def __init__( clss, name, bases, dictionary ):
+		def generateProperty( attrType, attrTag, attrName ):
+			"""
+			Because lambdas do not rememebr their scope, if the stack is overwritten.
+			"""
+			return ( lambda self: self.attrs[ attrTag ] ), ( lambda self, value: attrType( self, attrTag, attrName, value ) )
 
-        for attrTag, ( attrType, attrName, attrLinks ) in clss.attTypes.items( ):
-            get, set = generateProperty( attrType, attrTag, attrName )
-            prop = property( fget=get, fset=set, doc="This character's %s scor" % attrName )
-            setattr( clss, attrTag, prop )
-        super( CharacterMetaclass, clss ).__init__( clss, name, bases, dictionary )
+		for attrTag, ( attrType, attrName, attrLinks, baseVal ) in clss.attTypes.items( ):
+			get, set = generateProperty( attrType, attrTag, attrName )
+			prop = property( fget=get, fset=set, doc="This character's %s scor" % attrName )
+			setattr( clss, attrTag, prop )
+		super( CharacterMetaclass, clss ).__init__( clss, name, bases, dictionary )
 
 class Character( MyObject ):
-    lastkey = 0
+	lastkey = 0
 
-    def __init__( self, name, conditions, attrs=None ):
-        self.name = name
-        self.conditions = conditions
-        self.attrs = { }
-        self.key = "%s %s" % ( self.__class__.__name__, self.lastkey )
-        Character.lastkey += 1
-        self.setAttrs( attrs or { } )
-        self.setSystemAttrs( )
+	def __init__( self, name, attrs=None ):
+		self.name = name
+		self.attrs = { }
+		self.key = "%s %s" % ( self.__class__.__name__, self.lastkey )
+		Character.lastkey += 1
+		self.setAttrs( attrs or { } )
+		self.setSystemAttrs( )
 
-    def setAttrs( self, attrs ):
-        classattrs = dict.fromkeys( self.attTypes, 0 )
-        classattrs.update( attrs )
-        for name, attr in classattrs.items( ):
-            setattr( self, name, attr )
+	def setAttrs( self, attrs ):
+		classattrs = dict( [ ( key, baseVal ) for key, ( attType, attName, targetStrs, baseVal ) in self.attTypes.iteritems( ) ] )
+		classattrs.update( attrs )
+		for name, attr in classattrs.items( ):
+			setattr( self, name, attr )
 
-    def setSystemAttrs( self ):
-        """
-        Set the attributes that are specific to the system.  Since this is generic, we'll pass
-        """
-        for tag, ( attType, attName, targetStrs ) in self.attTypes.iteritems( ):
-            for modStr, targetCondition in targetStrs.iteritems( ):
-                target = self.attrs[ tag ]
-                mod = self.attrs[ modStr ].mod
-                if len( targetCondition ) > 0:
-                    condition = Conditions( targetCondition[ :-1 ], targetCondition[ -1 ] )
-                else:
-                    condition = Conditions( ( ), "and" )
-                mod.addTarget( target, condition )
+	def setSystemAttrs( self ):
+		"""
+		Set the attributes that are specific to the system.  Since this is generic, we'll pass
+		"""
+		for tag, ( attType, attName, targetStrs, baseVal ) in self.attTypes.iteritems( ):
+			for modStr, targetCondition in targetStrs.iteritems( ):
+				target = self.attrs[ tag ]
+				mod = self.attrs[ modStr ].mod
+				if len( targetCondition ) > 0:
+					condition = Conditions( targetCondition[ :-1 ], targetCondition[ -1 ] )
+				else:
+					condition = Conditions( ( ), "and" )
+				mod.addTarget( target, condition )
 
-    def __str__( self ):
-        return "%s\n\t%s\n" % ( self.name, "\n\t".join( [ str( attr ) for attr in self.attrs.values( ) ] ) )
+	def __str__( self ):
+		return "%s\n\t%s\n" % ( self.name, "\n\t".join( [ str( attr ) for attr in self.attrs.values( ) ] ) )
 
-    #Properties
-    def setStat( self, tag, name, number, modprovided=None ):
-        try:
-            self.attrs[ tag ].number = number
-        except:
-            self.attrs[ tag ] = Stat( name, number, self.conditions, modprovided )
+	#Properties
+	def setStat( self, tag, name, number, modprovided=None ):
+		try:
+			self.attrs[ tag ].number = number
+		except:
+			self.attrs[ tag ] = Stat( name, number, modprovided )
 
-    def setAttribute( self, tag, name, number ):
-        try:
-            self.attrs[ tag ].number = number
-        except:
-            self.attrs[ tag ] = Attribute( name, number, self.conditions )
+	def setAttribute( self, tag, name, number ):
+		try:
+			self.attrs[ tag ].number = number
+		except:
+			self.attrs[ tag ] = Attribute( name, number )
 
-    attTypes = { }
-    __metaclass__ = CharacterMetaclass
+	attTypes = { }
+	__metaclass__ = CharacterMetaclass
 
 if __name__ == "__main__":
-    import math
-    STR = Stat( "Strength", 14, conditions )
-    bob = Character( "Bob", conditions )
+	import math
+	STR = Stat( "Strength", 14, conditions )
+	bob = Character( "Bob", conditions )
