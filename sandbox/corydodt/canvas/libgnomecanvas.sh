c427b051fb9926075924aab0e313c8629d7d3f1c
corydodt <corydodt@dac6abc3-03f5-0310-8df6-960e0b1e0653>
2005-06-10 04:10:36 +0000

    bump point version of libgnomecanvas

--- sandbox/corydodt/canvas/libgnomecanvas.sh
+++ sandbox/corydodt/canvas/libgnomecanvas.sh
@@ -2,25 +2,24 @@
 set -e
 set -x
 
-echo This should be considered a Proof of Concept.  Use muntyan\'s
-echo libgnomecanvas binaries for real work.
+echo This should be considered a Proof of Concept.  Use muntyan\'s \
+libgnomecanvas binaries for real work.
 
 echo Can\'t have Cygwin\'s libintl in the static library search PATH
 [ ! -r /usr/lib/libintl.a -a ! -r /usr/lib/libintl.la -a ! -r /usr/lib/libintl.dll.a ] || exit 1
 
-CFLAGS="-mno-cygwin -mms-bitfields -I/target/include"
-PATH=/target/bin:"$PATH"
-ENV=env PATH="$PATH" CPPFLAGS="$CPPFLAGS" CFLAGS="$CFLAGS" 
+CFLAGS="-mno-cygwin -mms-bitfields -IC:/PROGRA~1/COMMON~1/GTK/include"
+ENV=env CPPFLAGS="$CPPFLAGS" CFLAGS="$CFLAGS" 
 
 # not using -mingw32 as the host causes the .dll to have the name
 # "cyggnomecanvas".  suck.
-$ENV PATH="$PATH" CFLAGS="$CFLAGS" ./configure --prefix="`pwd`/dist"
+$ENV CFLAGS="$CFLAGS" ./configure --prefix="`pwd`/dist"
 
 # using -mingw32 as the host causes .dll to not compile at all. suck. FIXME.
-#$ENV PATH="$PATH" CFLAGS="$CFLAGS" ./configure --prefix="`pwd`/dist" \
+#$ENV CFLAGS="$CFLAGS" ./configure --prefix="`pwd`/dist" \
 #                                               --build=i686-pc-mingw32 \
 #                                               --host=i686-pc-mingw32 \
 #                                               --target=i686-pc-mingw32
 
-$ENV PATH="$PATH" CFLAGS="$CFLAGS" make
-$ENV PATH="$PATH" CFLAGS="$CFLAGS" make install
+$ENV CFLAGS="$CFLAGS" make
+$ENV CFLAGS="$CFLAGS" make install
